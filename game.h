596401d9abd76a50d5328d5665856d27da77e7a0
bambaisbanned <Iphonegamer400@gmail.com>
2024-12-04 13:07:16 +0100

    Oh, i forgot this one!

--- game.h
+++ game.h
@@ -8,16 +8,14 @@
 class Game
 {
 public:
-    Game(Drawer& aDrawer);
-    void Management(sf::RenderWindow& MainWindow, sf::Event& event);
-    void KeyboardHandler(sf::RenderWindow& MainWindow, sf::Event& event);
+	Game(Drawer& aDrawer);
+	void Management(sf::RenderWindow& MainWindow, sf::Event& event);
+	void KeyboardHandler(sf::RenderWindow& MainWindow, sf::Event& event);
 
 private:
-    sf::Keyboard::Key KeyLane1;
-    sf::Keyboard::Key KeyLane2;
-    sf::Keyboard::Key KeyLane3;
-    sf::Keyboard::Key KeyLane4;
-    Drawer* drawer;
+	sf::Keyboard::Key KeyLane1;
+	sf::Keyboard::Key KeyLane2;
+	sf::Keyboard::Key KeyLane3;
+	sf::Keyboard::Key KeyLane4;
+	Drawer* drawer;
 };
-
-
